c983168a1a9fb17f050f8e00cfc95f8b9d0ca5dc
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-06-21 08:53:59 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                 Don't have an account ? <a href="{{ route('register') }}" class="font-500 text-primary">Signup now</a>
             </p>
             <p>
-                © 2020 Web Information Brebes <span class="d-none d-sm-inline-block">- Created by Syafiq Marzuki</span>
+                © 2020 Web Information Brebes <span class="d-none d-sm-inline-block"></span>
             </p>
         </div>
     </div>
