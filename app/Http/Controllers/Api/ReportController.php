8013ee79b5400951b6673f55769aa7266a912b3f
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-06-21 16:06:52 +0700

    update report

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -27,8 +27,14 @@ class ReportController extends Controller
     }
     public function monthnow(){
         $report = Report::select
-        (DB::raw('avg(debittumpah) as debittumpah, avg(sungai) as sungai, DATE(created_at) day'))
+        ('avg(debittumpah) as debittumpah, avg(sungai) as sungai, DATE(created_at) day')
             ->groupBy('day')->whereMonth('created_at','=', date('m'))->get();
+
+
+//        $report = Report::select
+//        (DB::raw('avg(debittumpah) as debittumpah, avg(sungai) as sungai, DATE(created_at) day'))
+//            ->groupBy('day')->whereMonth('created_at','=', date('m'))->get();
+
 
         return response()->json([
             'message' => 'berhasil',
